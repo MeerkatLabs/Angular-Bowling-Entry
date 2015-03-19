2c9df439c6e1fc337f11082c8862acbcbd781993
Robert Robinson <rerobins@gmail.com>
2015-03-19 10:38:45 -0600

    Delete League Functionality
    #27
    
    Code functionality for deleting leauges.  Still need unit tests to verify everything.

--- app/src/entry/core/league/LeagueDetailController.js
+++ app/src/entry/core/league/LeagueDetailController.js
@@ -1,9 +1,10 @@
 /**
  * Controller that will provide the entry point to the league details.
+ * @param $state
  * @param league
  * @constructor
  */
-var LeagueDetailController = function(league) {
+var LeagueDetailController = function($state, league) {
     var controller = this;
 
     controller.league = league;
@@ -12,10 +13,16 @@ var LeagueDetailController = function(league) {
         controller.substitutes = subs;
     });
 
+    controller.deleteLeague = function() {
+        league.remove().then(function() {
+            $state.go('^.^.leagues');
+        });
+    };
+
 };
 
 angular.module('bowling.entry.core')
-    .controller('LeagueDetailController', ['league', LeagueDetailController])
+    .controller('LeagueDetailController', ['$state', 'league', LeagueDetailController])
     .config(['$stateProvider', function($stateProvider) {
 
         $stateProvider.state('bowling.league.detail', {
